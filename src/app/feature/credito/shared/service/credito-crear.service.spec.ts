a7a7ddfdae19cc4764b1a1aa149cc3983ada5cab
Duval <deledesman@gmail.com>
2022-03-04 14:34:51 -0500

    Correccion de pruebas

--- src/app/feature/credito/shared/service/credito-crear.service.spec.ts
+++ src/app/feature/credito/shared/service/credito-crear.service.spec.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 import { CreditoCrearService } from './credito-crear.service';
 
 describe('Service: Credito', () => {
--- src/app/feature/credito/shared/service/credito-listar.service.spec.ts
+++ src/app/feature/credito/shared/service/credito-listar.service.spec.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 import { CreditoListarService } from './credito-listar.service';
 
 describe('Service: CreditoListar', () => {
