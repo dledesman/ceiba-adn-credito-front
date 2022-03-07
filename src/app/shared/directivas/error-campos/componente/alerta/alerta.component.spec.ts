d2c29078e5130365360d98a8b0d40336c4d11b48
Duval <deledesman@gmail.com>
2022-03-07 07:39:41 -0500

    Eliminar clases deprecadas

--- src/app/shared/directivas/error-campos/componente/alerta/alerta.component.spec.ts
+++ src/app/shared/directivas/error-campos/componente/alerta/alerta.component.spec.ts
@@ -1,7 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AlertaComponent } from './alerta.component';
 
@@ -9,7 +7,7 @@ describe('AlertaComponent', () => {
   let component: AlertaComponent;
   let fixture: ComponentFixture<AlertaComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AlertaComponent ]
     })
